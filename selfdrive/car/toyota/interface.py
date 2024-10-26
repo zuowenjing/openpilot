73e21e92b06bce5820dcc31a1199b7a67a041438
zuowenjing <41935404+zuowenjing@users.noreply.github.com>
2024-10-26 09:59:05 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -143,8 +143,8 @@ class CarInterface(CarInterfaceBase):
     if candidate in TSS2_CAR or ret.flags & ToyotaFlags.NEW_TOYOTA_TUNE:
       tune.kpV = [0.0]
       tune.kiV = [0.5]
-      ret.vEgoStopping = 0.25
-      ret.vEgoStarting = 0.25
+      ret.vEgoStopping = 0.15
+      ret.vEgoStarting = 0.05
       ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
 
       # Since we compensate for imprecise acceleration in carcontroller, we can be less aggressive with tuning
